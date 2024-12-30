c10c99a743dac1a89f9985eacc7ab5a1a9007c87
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2024-12-30 12:23:45 -0800

    pagina actualizada

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 window.addEventListener('scroll', () => {
-    const header = document.querySelector('.site-header');
-    if (window.scrollY > 50) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
+  const header = document.querySelector('.site-header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mySwiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 5000,
+      // disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
   });
+});
